48b516695dcc0275cd6ad48a740cc08597af56c4
Matt Graham <mattg@rhombussystems.com>
2022-04-01 11:35:13 -0700

    initial commit

---
+++ src/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDpIb_MwUJgcI9_kSKgPofugatJ2gX2ZpQ",
+  authDomain: "arcade-matthewgraham-me.firebaseapp.com",
+  databaseURL: "https://arcade-matthewgraham-me.firebaseio.com",
+  projectId: "arcade-matthewgraham-me",
+  storageBucket: "arcade-matthewgraham-me.appspot.com",
+  messagingSenderId: "228000157058",
+  appId: "1:228000157058:web:7eb9ed2091dc5f0edd539b",
+  measurementId: "G-5S6GJMRB4Q",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
